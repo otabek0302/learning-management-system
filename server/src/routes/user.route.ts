57e4272e3f1fbd15c84a4ca5ac5a8b1678a57788
Otabek <otabekjon2002@mail.ru>
2024-12-20 05:14:50 +0500

    in this commit: logout of user done

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,10 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { activateUser, loginUser, resgisterUser } from "../controller/user.controller";
+import { activateUser, loginUser, logoutUser, resgisterUser } from "../controller/user.controller";
 
 router.post('/registration', resgisterUser);
+
 router.post('/activate-user', activateUser);
+
 router.post('/login-user', loginUser);
+
+router.get('/logout', logoutUser);
 
 export default router;
