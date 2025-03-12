7e118a17e583b2e4bdccba0ba1b749b557d17923
Otabek <otabekjon2002@mail.ru>
2025-03-13 03:51:41 +0500

    updated: couse model | created: interface file for course

---
+++ server/src/interfaces/course.interface.ts
@@ -0,0 +1,54 @@
+import { Document } from "mongoose";
+
+// Comments
+export interface IComment extends Document {
+    user: string;
+    comment: string;
+    createdAt: Date;
+    commentReplies: IComment[];
+}
+
+// Reviews
+export interface IReview extends Document {
+    user: string;
+    rating: number;
+    comment: string;
+}
+
+// Links
+export interface ILink extends Document {
+    title: string;
+    url: string;
+}
+
+// Course Data
+export interface ICourseData extends Document {
+    title: string;
+    description: string;
+    videoUrl: string;
+    videoThumbnail: string;
+    videoSection: string;
+    videoLength: number;
+    videoPlayer: string;
+    links: ILink[];
+    suggestion: string;
+    questions: IComment[];
+}
+
+// Course
+export interface ICourse extends Document {
+    name: string;
+    description: string;
+    price: number;
+    estimatedPrice?: number;
+    thumbnail: object;
+    tags: string;
+    level: string;
+    demoUrl: string;
+    benefits: { title: string }[];
+    prerequisites: { title: string }[];
+    reviews: IReview[];
+    courseData: ICourseData[];
+    ratings: number;
+    purchased: number;
+}
--- server/src/models/course.model.ts
+++ server/src/models/course.model.ts
@@ -1,64 +1,7 @@
-import { Document, Model, Schema, model } from "mongoose";
-
-
-
-// Comments
-interface IComment extends Document {
-    user: string;
-    comment: string;
-    createdAt: Date;
-    commentReplies: IComment[];
-}
-
-// Reviews
-interface IReview extends Document {
-    user: string;
-    rating: number;
-    comment: string;
-    commentReplies: IComment[];
-}
-
-// Links
-interface ILink extends Document {
-    title: string;
-    url: string;
-}
-
-// Course Data
-interface ICourseData {
-    title: string;
-    description: string;
-    videoUrl: string;
-    videoThumbnail: string;
-    videoSection: string;
-    videoLength: number;
-    videoPlayer: string;
-    links: ILink[];
-    suggestion: string;
-    questions: IComment[];
-}
-
-// Course
-interface ICourse extends Document {
-    name: string;
-    description: string;
-    price: number;
-    estimatedPrice?: number;
-    thumbnail: object;
-    tags: string;
-    level: string;
-    demoUrl: string;
-    benefits: { title: string }[];
-    prerequisites: { title: string }[];
-    reviews: IReview[];
-    courseData: ICourseData[];
-    ratings: number;
-    purchased: number;
-}
-
+import { Model, Schema, model } from "mongoose";
+import { IComment, IReview, ILink, ICourseData, ICourse } from "../interfaces/course.interface";
 
 // Course Schema  //
-
 const reviewSchema = new Schema<IReview>({
     user: Object,
     rating: {
@@ -77,7 +20,7 @@ const commentSchema = new Schema<IComment>({
     user: Object,
     comment: String,
     commentReplies: [Object],
-});
+}, { timestamps: true });
 
 const courseDataSchema = new Schema<ICourseData>({
     title: String,
